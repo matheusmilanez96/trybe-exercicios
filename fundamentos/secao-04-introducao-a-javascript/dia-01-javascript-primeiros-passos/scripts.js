11b8bd5a6ecd60e83116d0958751a5ddb2143684
Matheus Milanez <mattmila96@gmail.com>
2022-10-20 15:10:44 -0300

    começando a seção 4

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,13 @@
+const myName = "Matheus";
+const birthCity = "Campinas";
+let birthYear = 1996;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "Osasco";
+console.log(birthCity)
