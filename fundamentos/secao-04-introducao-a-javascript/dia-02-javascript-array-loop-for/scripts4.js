f2ff786f6cc86838fe9115ef0caa37e944b4f1d6
Matheus Milanez <mattmila96@gmail.com>
2022-10-21 18:20:35 -0300

    continuando

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-loop-for/scripts4.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-loop-for/scripts4.js
@@ -13,3 +13,28 @@ console.log(soma);
 //média
 let media = soma / numbers.length;
 console.log(media);
+
+//valor final
+if (numbers[numbers.length - 1] > 20) {
+    console.log("valor maior que 20")
+} else {
+    console.log("valor menor or igual a 20")
+}
+
+//maior valor
+let maiorNumero = numbers[0];
+for (let j = 1; j < numbers.length; j += 1) {
+    if (numbers[j] > maiorNumero) {
+        maiorNumero = numbers[j];
+    }
+}
+console.log("Maior valor: " + maiorNumero)
+
+//valores ímpares
+let totalImpares = 0
+for (let k = 0; k < numbers.length; k += 1) {
+    if (numbers[k] % 2 != 0) {
+        totalImpares += 1;
+    }
+}
+console.log("Total de impares: " + totalImpares)
