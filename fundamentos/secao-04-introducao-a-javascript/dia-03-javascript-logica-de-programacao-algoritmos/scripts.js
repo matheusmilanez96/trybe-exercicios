6dba78f119f1656453e93b9af2a0cf688447bd5e
Matheus Milanez <mattmila96@gmail.com>
2022-10-24 17:42:32 -0300

    segundo exercício

--- fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-algoritmos/scripts.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-algoritmos/scripts.js
@@ -1,7 +1,16 @@
 // fatorial de 10
 
 let total = 1;
-for (let n = 1; n <= 10; n += 1) {
+let fatorial = 10;
+for (let n = 1; n <= fatorial; n += 1) {
     total = total * n;
 }
 console.log(total);
+
+//inverter palavra
+let word = "tryber"
+let palavraInvertida = "";
+for (let i = 1; i <= word.length; i += 1) {
+    palavraInvertida += word[word.length - i];
+}
+console.log(palavraInvertida);
