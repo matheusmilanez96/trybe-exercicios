f815ef89a1a6a832168c7945ba6f35183ba97ba3
Matheus Milanez <mattmila96@gmail.com>
2022-10-24 18:00:05 -0300

    terceiro exercício

--- fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-algoritmos/scripts.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-algoritmos/scripts.js
@@ -14,3 +14,22 @@ for (let i = 1; i <= word.length; i += 1) {
     palavraInvertida += word[word.length - i];
 }
 console.log(palavraInvertida);
+
+//maior e menor palavras
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let indexMaior = 0;
+let indexMenor = 0;
+
+for (let j = 0; j < array.length; j += 1) {
+    if (array[j].length > array[indexMaior].length) {
+        indexMaior = j;
+    }
+}
+console.log(array[indexMaior]);
+
+for (let k = 0; k < array.length; k += 1) {
+    if (array[k].length < array[indexMenor].length) {
+        indexMenor = k;
+    }
+}
+console.log(array[indexMenor]);
