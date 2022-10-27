aae35bcc42f401d5d85731e145b8d4219a0d96ed
Matheus Milanez <mattmila96@gmail.com>
2022-10-27 11:41:44 -0300

    exercício ternary operator

--- fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6/scripts.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6/scripts.js
@@ -20,3 +20,7 @@ const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`
 let nome = 'Ivan';
 let sobrenome = 'Pires';
 console.log(nomeCompleto(nome, sobrenome));
+
+let speed = 90;
+const speedCar = (speed) => speed >= 120 ? `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida`;
+console.log(speedCar(speed));
