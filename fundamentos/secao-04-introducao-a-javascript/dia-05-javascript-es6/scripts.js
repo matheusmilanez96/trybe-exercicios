c0a603bf5d7b6da481024410bda2c8c108fc6dcb
Matheus Milanez <mattmila96@gmail.com>
2022-10-27 11:39:54 -0300

    exercícios arrow functions

--- fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6/scripts.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6/scripts.js
@@ -8,3 +8,15 @@ pessoa = {
 }
 console.log('Nome:', pessoa.nome);
 console.log('Idade:', pessoa.idade);
+
+const numeroAleatorio = () => Math.random()
+console.log(numeroAleatorio());
+
+//const hello = (nome) => `Olá, ${nome}!`
+//let nome = 'Ivan';
+//console.log(hello(nome));
+
+const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`
+let nome = 'Ivan';
+let sobrenome = 'Pires';
+console.log(nomeCompleto(nome, sobrenome));
