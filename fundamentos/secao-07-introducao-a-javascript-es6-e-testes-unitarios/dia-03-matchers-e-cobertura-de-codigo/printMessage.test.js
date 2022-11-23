daf72e40b01183c223efb22c2e47107a587efafe
Matheus Milanez <mattmila96@gmail.com>
2022-11-22 22:49:06 -0300

    terceiro exercício

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/printMessage.test.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/printMessage.test.js
@@ -1,9 +1,14 @@
 const {
     info,
+    printMessage,
 } = require('./printMessage');
 
 describe('a função printMessage', () => {
-    it('testa se o objeto possui o parâmetro', () => {
-        expect(info).toHaveProperty('personagem');
-    });
+  it('testa se o objeto possui o parâmetro', () => {
+    expect(info).toHaveProperty('personagem');
+  });
+  it('testa se o retorno contém Boas Vindas', () => {
+    expect(printMessage(info)).toMatch('Boas vindas,');
+    expect(printMessage(info)).toMatch(info.personagem);
+  });
 });
