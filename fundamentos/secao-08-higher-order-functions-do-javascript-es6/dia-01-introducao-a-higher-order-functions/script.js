d18147fb642c4eae966050d68a6d62cd3d549381
Matheus Milanez <mattmila96@gmail.com>
2022-11-30 00:56:31 -0300

    ex 2

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/script.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/script.js
@@ -1,16 +1,25 @@
 const employeeGenerator = (fullName) => {
-    const email = fullName.toLowerCase().replace(' ', '_');
-    return { fullName, email: `${email}@trybe.com` };
+  const email = fullName.toLowerCase().replace(' ', '_');
+  return { fullName, email: `${email}@trybe.com` };
+};
+  
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'),
+    id2: callback('Luiza Drumond'),
+    id3: callback('Carla Paiva'),
   };
+  return employees;
+};
   
-  const newEmployees = (callback) => {
-    const employees = {
-      id1: callback('Pedro Guerra'),
-      id2: callback('Luiza Drumond'),
-      id3: callback('Carla Paiva'),
-    };
-    return employees;
-  };
-  
-  console.log(newEmployees(employeeGenerator));
-  
+console.log(newEmployees(employeeGenerator));
+
+const numberChecker = (myNumber, number) => myNumber === number;
+
+const lotteryResult = (myNumber, callback) => {
+  const number = Math.floor((Math.random() * 5) + 1);
+
+  return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try Again!';
+};
+
+console.log(lotteryResult(2, numberChecker));
