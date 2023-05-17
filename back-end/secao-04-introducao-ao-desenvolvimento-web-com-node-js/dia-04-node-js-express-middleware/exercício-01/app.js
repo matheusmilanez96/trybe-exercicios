94e51801c667695d92ccfef018ef1c1995e7477b
Matheus Milanez <mattmila96@gmail.com>
2023-05-17 13:51:07 -0300

    ex 2 finalizado

--- back-end/secao-04-introducao-ao-desenvolvimento-web-com-node-js/dia-04-node-js-express-middleware/exercício-01/app.js
+++ back-end/secao-04-introducao-ao-desenvolvimento-web-com-node-js/dia-04-node-js-express-middleware/exercício-01/app.js
@@ -1,5 +1,6 @@
 // app.js
 const express = require('express');
+const auth = require('./middlewares/auth');
 const validateDifficulty = require('./middlewares/validateDifficulty');
 const validateRating = require('./middlewares/validateRating');
 const validateCreatedAt = require('./middlewares/validateCreatedAt');
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 
 app.post('/activities',
+  auth,
   validateDifficulty,
   validateRating,
   validateCreatedAt,
