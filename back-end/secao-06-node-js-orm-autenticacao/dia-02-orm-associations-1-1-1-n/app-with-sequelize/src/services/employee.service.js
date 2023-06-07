7b4fbb9a396906757b5d000b3a18d1c9b85be639
Matheus Milanez <mattmila96@gmail.com>
2023-06-07 15:13:28 -0300

    seção 6

---
+++ back-end/secao-06-node-js-orm-autenticacao/dia-02-orm-associations-1-1-1-n/app-with-sequelize/src/services/employee.service.js
@@ -0,0 +1,27 @@
+// src/services/employee.service.js
+
+const { Address, Employee } = require('../models/');
+
+const getAll = async () => {
+  const users = await Employee.findAll({
+    include: { model: Address, as: 'addresses' },
+  });
+
+  return users;
+};
+
+const getById = async (id) => {
+  const employee = await Employee.findOne({
+    where: { id },
+  });
+  return employee;  
+}
+
+const insert = async ({ firstName, lastName, age, city, street, number }) => {
+  const employee = await Employee.create({ firstName, lastName, age });
+
+  await Address.create({ city, street, number, employeeId: employee.id });
+  return employee;
+};
+
+module.exports = { getAll, getById, insert };
