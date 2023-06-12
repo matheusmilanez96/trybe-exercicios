1f26cf16d096d751d65e24ed619fc09a3339e306
Matheus Milanez <mattmila96@gmail.com>
2023-06-12 00:42:18 -0300

    exs 6-11

--- back-end/secao-06-node-js-orm-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-01/controllers/BooksController.js
+++ back-end/secao-06-node-js-orm-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-01/controllers/BooksController.js
@@ -5,6 +5,23 @@ const getAll = async (req, res) => {
   res.status(200).json(books);
 };
 
+const getById = async (req, res) => {
+  const book = await BookService.getById(req.params.id);
+
+  if (!book) return res.status(404).json({ message: 'Book not found' });
+
+  res.status(200).json(book);
+};
+
+const create = async (req, res) => {
+  const { title, author, pageQuantity } = req.body;
+  const book = await BookService.create({ title, author, pageQuantity });
+
+  res.status(201).json(book);
+};
+
 module.exports = {
   getAll,
+  getById,
+  create,
 };
--- back-end/secao-06-node-js-orm-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-01/services/BookService.js
+++ back-end/secao-06-node-js-orm-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-01/services/BookService.js
@@ -5,6 +5,18 @@ const getAll = async () => {
   return books;
 };
 
+const getById = async (id) => {
+  const book = await Book.findByPk(id);
+  return book;
+};
+
+const create = async ({ title, author, pageQuantity }) => {
+  const book = await Book.create({ title, author, pageQuantity });
+  return book;
+};
+
 module.exports = {
   getAll,
+  getById,
+  create,
 };
